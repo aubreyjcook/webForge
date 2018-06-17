f56cbe24ae1c41ce54213a7630fb1b62583fdeb4
Aubrey Cook <aubreyjcook@gmail.com>
2018-06-17 04:28:26 -0500

    Importing boilerplate

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <?php include 'php/head-meta.php';?>
+        <?php include 'php/conditionalPageLoading.php';?>
+        <?php include 'php/head-links.php';?>
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        <div class="container">
+          <header class="grid-item">
+            <div class="grid-item">
+              <figure>
+                <h1>Figure</h1>
+              </figure>
+              <figcaption>
+                Lorem ipsum dolor sit amet.
+              </figcaption>
+            </div>
+            <nav class="grid-item">
+              <h1>Nav Header</h1>
+              <ul>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <main class="grid-item">
+            <div class="grid-item">
+              <h1>Main Header</h1>
+            </div>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+            <section class="grid-item">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </section>
+          </main>
+          <footer class="grid-item">
+            <section class="grid-item">
+              <h1>Footer Section 1</h1>
+            </section>
+            <section class="grid-item">
+              <h1>Footer Section 2</h1>
+            </section>
+            <section class="grid-item">
+              <h1>Footer Section 3</h1>
+            </section>
+          </footer>
+        </div>
+        <?php include 'php/body-scripts.php';?>
+    </body>
+</html>
